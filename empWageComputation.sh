2ba630a19922f74b1874dda8da65d86c711c94c1
JaswinderSinghdigwa <findjassi121212@gmail.com>
2021-07-16 12:06:05 +0530

    modified empwages attendances features

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,14 @@
 #! /bin/bash -x
+
+
+Present=0
+Notpresent=1
+
+if [ ((RANDOM%2 )) -eq $Present ]
+then
+echo "employee is present"
+else
+echo "employee is not  present"
+fi
+
 echo "Welcometo employee problem"
